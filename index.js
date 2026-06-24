cf5e7927a3018fa34bc333f10c22da1568795397
chuyesta187-beep <chuyesta187@gmail.com>
2026-06-23 19:34:41 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`🤖 Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN); 
